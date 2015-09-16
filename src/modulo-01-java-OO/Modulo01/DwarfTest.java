365f58ceeb9aa13772ec7cca7564d807fa7efa6e
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-09-16 17:35:16 -0300

    feat(DwarfTest): teste unitário para nascimento do Dwarf.

---
+++ src/modulo-01-java-OO/Modulo01/DwarfTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DwarfTest
+{
+    @Test
+    public void dwarfNasceCom110DeVida() {
+        Dwarf gimli = new Dwarf();
+        assertEquals(110, gimli.getVida());
+    }
+}
