644d704d90ee8f90b86fce27634a43752775aec2
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-09-16 15:38:39 -0300

    feat(TemaDia02): versionar tema do dia 02.

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,9 +1,58 @@
 public class Elfo {
     private String nome;
-    private int flechas;
+    private int flechas, experiencia;
     
-    public Elfo(String n, int flech) {
-        nome = n;
-        flechas = flech;
+    /* Type initializer
+     * Executa antes de cada construtor
+    {
+        flechas = 42;
+    }
+    */
+    public Elfo(String nome, int flechas) {
+        this.nome = nome;
+        this.flechas = flechas;
+    }
+    
+    public Elfo(String nome, Integer flechas) {
+        this(nome);
+        if (flechas == null) {
+            this.flechas = 89;
+        }
+    }
+    
+    public Elfo(String nome) {
+        this(nome, 42);
+    }
+
+    /* PascalCase (C#, VB.NET)
+     *      public void AtirarFlechaDeFogo
+     * camelCase (Java, JavaScript)
+     *      public void atirarFlechaDeFogo
+    */
+   
+    public void atirarFlecha() {
+        flechas--;
+        experiencia++;
+        //experiencia += 1;
+        //experiencia = experiencia + 1;
+    }
+    
+    /*
+     * 
+     * public atirarFlechaRefactory(this.flechas, this.experiencia){
+     *     if(boolean acertar == true){
+     *         flechas--;
+     *         experiencia++;
+     *      }else{
+     *          flechas--;
+     *      }
+     *  }
+     */
+    public void atirarFlechaRefactory(){
+        boolean acertar = true;
+        if (acertar) {
+            experiencia++;
+        }
+        flechas--;
     }
 }
