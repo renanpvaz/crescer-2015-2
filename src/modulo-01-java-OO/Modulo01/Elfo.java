e2d5dde8a890cfd5b8303f1bab5de5017adbe60b
Renan Vaz <renanp.vaz@outlook.com>
2015-09-16 13:06:23 -0300

    no message

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,9 +1,23 @@
 public class Elfo {
     private String nome;
-    private int flechas;
+    private Integer flechas;
+    private int experiencia = 0;
     
-    public Elfo(String n, int flech) {
+    public Elfo(String n, Integer flech) {
         nome = n;
         flechas = flech;
+        if(flech == null){
+            flechas = 42;
+        }
+    }
+    
+    public void atirarFlecha(){
+        experiencia++;
+	flechas--;
     }
 }
+    
+    
+    
+    
+   
