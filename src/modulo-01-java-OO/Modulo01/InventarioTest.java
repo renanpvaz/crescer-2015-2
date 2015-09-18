7fed3302f8571e58e633f5e169a6ba2dd93056b0
Renan Vaz <renanp.vaz@outlook.com>
2015-09-18 20:27:34 -0300

    test(InventarioTest): adicionado testes para Inventario

---
+++ src/modulo-01-java-OO/Modulo01/InventarioTest.java
@@ -0,0 +1,34 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class InventarioTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class InventarioTest
+{
+   @Test
+   public void itemAdicionadoPosição1(){
+       Inventario i = new Inventario();
+       Item batata = new Item("Batata", 3);
+       i.adicionarItem(batata);
+       assertEquals(i.getListaDeItens().get(0), batata);
+   }
+   
+   @Test
+   public void itemRemoverPosição1(){
+       Inventario i = new Inventario();
+       Item batata = new Item("Batata", 3);
+       Item batataDoce = new Item("Batata Doce", 1);
+       i.adicionarItem(batata);
+       i.adicionarItem(batataDoce);
+       i.removerItem(batataDoce);
+       assertEquals(1, i.getListaDeItens().size());
+   }
+}
