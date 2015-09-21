e6b65e06f4669ed2ac1dd343336c6973946f1da7
Renan Vaz <renanp.vaz@outlook.com>
2015-09-21 16:57:33 -0300

    feat(Item): adicionar método adicionar1000MaisSoma

--- src/modulo-01-java-OO/Modulo01/Inventario.java
+++ src/modulo-01-java-OO/Modulo01/Inventario.java
@@ -1,65 +1,68 @@
 import java.util.ArrayList;
 public class Inventario
 {
-   ArrayList<Item> ListaDeItens = new ArrayList<Item>();
-   
-   public ArrayList<Item> getListaDeItens(){
-       return ListaDeItens;
-   }
-   
-   public void adicionarItem(Item item){
-       ListaDeItens.add(item); 
-   }
-    
-   public void removerItem(Item item){
-       ListaDeItens.remove(item);
-   }
-   
-   public String getDescricoesItens(){
-      int index = this.getListaDeItens().size();
-      String descricoes = "";
-      for(int i = 0; i < index; i++){
-          if(i == index - 1){
-              descricoes += this.getListaDeItens().get(i).getDescricao();
-          }else{
-              descricoes += this.getListaDeItens().get(i).getDescricao() + ",";
-          }
-      }
-      return descricoes;
+    ArrayList<Item> ListaDeItens = new ArrayList<Item>();
+
+    public ArrayList<Item> getListaDeItens(){
+        return ListaDeItens;
+    }
+
+    public void adicionarItem(Item item){
+        ListaDeItens.add(item); 
+    }
+
+    public void removerItem(Item item){
+        ListaDeItens.remove(item);
+    }
+
+    public String getDescricoesItens(){
+        String descricoes = "";
+
+        for(Item item : this.ListaDeItens){
+            descricoes += item.getDescricao() + ",";
+        }
+        return descricoes.substring(0, descricoes.length() - 1);
+    }
+
+    public void adicionar1000TodosItens(){
+        int i = 0;
+        for(Item item : this.ListaDeItens){
+            this.ListaDeItens.get(i).adicionar1000();
+            i++;
+        }
     }
     
-   public void adicionar1000TodosItens(){
-    int i = 0;
-    while (i < this.ListaDeItens.size()){
-        this.ListaDeItens.get(i).adicionar1000();
-        i++;
+    public void adicionar1000TodosItensMaisSoma(){
+        int i = 0;
+        for(Item item : this.ListaDeItens){
+            this.ListaDeItens.get(i).adicionar1000MaisSoma();
+            i++;
+        }
     }
-   }
-   
-   public Item getItemComMaiorQuantidade(){
-       Item itemComMaiorQuantidade = this.ListaDeItens.get(0);
-       int qntMaior = 0;
-       for(int i = 0; i < this.ListaDeItens.size(); i++){
-           if(this.ListaDeItens.get(i).getQuantidade() > qntMaior){
-               qntMaior = this.ListaDeItens.get(i).getQuantidade();
-               itemComMaiorQuantidade = this.ListaDeItens.get(i);
+
+    public Item getItemComMaiorQuantidade(){
+        Item itemComMaiorQuantidade = this.ListaDeItens.get(0);
+        int qntMaior = 0;
+        for(Item item : this.ListaDeItens){
+            if(item.getQuantidade() > qntMaior){
+                qntMaior = item.getQuantidade();
+                itemComMaiorQuantidade = item;
             }
         } 
-       return itemComMaiorQuantidade;
-   }
-   
-   public void ordenarItens(){
-       Item maior;
+        return itemComMaiorQuantidade;
+    }
+
+    public void ordenarItens(){
+        Item maior;
         for(int i = 1; i < ListaDeItens.size(); i++){
             for(int ii = 0; ii < ListaDeItens.size(); ii++){
                 if(ListaDeItens.get(ii).getQuantidade() > ListaDeItens.get(i).getQuantidade()){
-                        maior = ListaDeItens.get(ii);
-                        ListaDeItens.set(ii, ListaDeItens.get(i));
-                        ListaDeItens.set(i, maior);
-                    }
+                    maior = ListaDeItens.get(ii);
+                    ListaDeItens.set(ii, ListaDeItens.get(i));
+                    ListaDeItens.set(i, maior);
                 }
             }
         }
-   }     
-
+    }
+}     
 
--- src/modulo-01-java-OO/Modulo01/Item.java
+++ src/modulo-01-java-OO/Modulo01/Item.java
@@ -7,23 +7,41 @@
  */
 public class Item
 {
-   private String descricao;
-   private int quantidade;
-   
-   public Item(String descricao, int quantidade){
-       this.descricao = descricao;
-       this.quantidade = quantidade;
-   }
-   
-   public String getDescricao(){
-       return descricao;
-   }
-   
-   public int getQuantidade(){
-       return quantidade;
-   }
-   
-   public void adicionar1000(){
-       this.quantidade += 1000;
-   }
+    private String descricao;
+    private int quantidade;
+
+    public Item(String descricao, int quantidade){
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    public void adicionar1000MaisSoma(){
+        /*int n = this.quantidade;
+        n = (n * (n + 1))/2;
+        this.quantidade += n + n * 1000;
+         */
+
+        /*Original:
+         * int pa = this.quantidade * ++this.quantidade / 2;
+         * this.quantidade = (1000 + this.quantidade * pa);
+         * 
+         * Correção:
+         * int pa = (this.quantidade * (this.quantidade + 1)) / 2;
+         * this.quantidade += (pa + pa) * 1000;
+         */
+        int pa = (this.quantidade * (this.quantidade + 1)) / 2;
+        this.quantidade += pa + pa * 1000;
+    }
+
+    public void adicionar1000(){
+        this.quantidade += 1000;
+    }
 }
