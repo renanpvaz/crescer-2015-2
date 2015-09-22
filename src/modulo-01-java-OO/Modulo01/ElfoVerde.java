305a65859c3980f6250b1cdbac401c8600c06ad7
Renan Vaz <renanp.vaz@outlook.com>
2015-09-22 17:27:29 -0300

    feat(ElfoVerde): estender Elfo

--- src/modulo-01-java-OO/Modulo01/ElfoVerde.java
+++ src/modulo-01-java-OO/Modulo01/ElfoVerde.java
@@ -5,29 +5,9 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ElfoVerde
+public class ElfoVerde extends Elfo
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class ElfoVerde
-     */
-    public ElfoVerde()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public ElfoVerde(){
+        
     }
 }
