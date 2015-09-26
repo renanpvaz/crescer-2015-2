f330a193a42ae7895ba66680ca22afd369f565e7
Renan Vaz <renanp.vaz@outlook.com>
2015-09-26 18:15:35 -0300

    test(EstrategiaAntiNoturnos): adicionar testes para EstrategiaAntiNoturnos

--- src/modulo-01-java-OO/Modulo01/EstrategiaAntiNoturnos.java
+++ src/modulo-01-java-OO/Modulo01/EstrategiaAntiNoturnos.java
@@ -1,14 +1,43 @@
 import java.util.*;
 public class EstrategiaAntiNoturnos implements EstrategiaDeAtaque{
-    
+     ArrayList<Elfo> ordemDoUltimoAtaque;
     //TO DO:
-    public void atacar(ExercitoDeElfos exercitoDeElfos, ArrayList<Dwarf> orcs){
-        exercitoDeElfos.ordenarDeixandoElfosNoturnosPorUltimo();
+    public void atacar(ExercitoDeElfos exercitoDeElfos, ArrayList<Dwarf> dwarves){
+        ordemDoUltimoAtaque = new ArrayList<Elfo>();
+        exercitoDeElfos.agruparPorStatus();
         ArrayList<Elfo> exercito = exercitoDeElfos.buscar(Status.VIVO);
-        for(Elfo elfo : exercito){    
-            for(Dwarf dwarf : orcs){
-                    elfo.atirarFlecha(dwarf);
+        int count = 0;
+        for(Elfo elfo : exercito){
+            if(elfo instanceof ElfoNoturno){
+                count++; 
+                if(count >= (int)(exercito.size() * dwarves.size()) * 0.3){
+                    continue;
                 }
+            }
+            for(Dwarf dwarf : dwarves){
+                    elfo.atirarFlecha(dwarf);
+             }
+            ordemDoUltimoAtaque.add(elfo);
         }
     }
+    
+    /*
+    public void atacar(ExercitoDeElfos exercitoDeElfos, ArrayList<Dwarf> dwarves){
+        exercitoDeElfos.agruparPorStatus();
+        ArrayList<Elfo> exercito = exercitoDeElfos.ordenarNoturnosNoFinal();
+        for(Elfo elfo : exercito){
+            if(elfo.getStatus() == Status.MORTO){
+                continue;
+            }
+            for(Dwarf dwarf : dwarves){
+                    elfo.atirarFlecha(dwarf);
+            }
+            ordemDoUltimoAtaque.add(elfo);
+        }
+    }
+    */
+    
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return this.ordemDoUltimoAtaque;
+    }  
 }
