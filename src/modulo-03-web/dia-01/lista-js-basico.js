69034b948a0dd2c9257c0f712293358cdd54c6a9
Renan Vaz <renanp.vaz@outlook.com>
2015-10-19 15:20:10 -0200

    resolução exercício 6

--- src/modulo-03-web/dia-01/lista-js-basico.js
+++ src/modulo-03-web/dia-01/lista-js-basico.js
@@ -28,14 +28,9 @@ function fiboSum(i){
 
 //Exercício 5
 function excelis(a){
-  var code = 0;
-  var code2 = 0;
-  for (var i in a){
-    if(i.equals(0){
-     code += (a[i].charCodeAt() - 64);
-   }else{
-     code2 += (a[i].charCodeAt() - 38);
-   }
-  }
-  return a.length === 1? code : (code * code2) - ((a[0].charCodeAt() - 65) * (a[1].charCodeAt() - 64));
-}
+    var b = 0;
+    for(var i = 0; i < a.length; i++){
+      b = b * 26 + (a.charCodeAt(i) - 64);
+    }
+    return b;
+  };
