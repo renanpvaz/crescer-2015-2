f7be762e884d451d761ba00da6c228d591fe38b8
Renan Vaz <renanp.vaz@outlook.com>
2015-10-20 01:22:03 -0200

    fix exercício 1 e 2

--- src/modulo-03-web/dia-01/exercicios-colecoes-js.js
+++ src/modulo-03-web/dia-01/exercicios-colecoes-js.js
@@ -1,48 +1,23 @@
 //Exercício 1.A
 function ordenaPorNacionais(arr){
-  var aux;
-  for(var i = 0; i < arr.length - 1; i++){
-    if(arr[i + 1].titulos[0].qtd > arr[i].titulos[0].qtd){
-      aux = arr[i + 1];
-      arr[i + 1] = arr[i];
-      arr[i] = aux;
-    }
-  }
+    arr.sort(function(a, b){ return a.titulos[0].qtd < b.titulos[0].qtd});
 }
 
-/*
-*  function ordenaPorNacionais(arr){
-*    arr.sort(function(a, b){ return a.titulos[0].qtd < b.titulos[0].qtd ? b : a; });
-*  }
-*/
 
 //Exercício 1.B
 function ordenaPorContinentais(arr){
-  var aux;
-  for(var i = 0; i < arr.length - 1; i++){
-    if(arr[i + 1].titulos[1].qtd > arr[i].titulos[1].qtd){
-      aux = arr[i + 1];
-      arr[i + 1] = arr[i];
-      arr[i] = aux;
-    }
-  }
+  arr.sort(function(a, b){ return a.titulos[1].qtd < b.titulos[1].qtd});
 }
 
 //Exercício 1.C
-function ordenaPorContinentais(arr){
-  var aux;
-  for(var i = 0; i < arr.length - 1; i++){
-    if(arr[i + 1].titulos[2].qtd > arr[i].titulos[2].qtd){
-      aux = arr[i + 1];
-      arr[i + 1] = arr[i];
-      arr[i] = aux;
-    }
-  }
+function ordenaPorMundiais(arr){
+  arr.sort(function(a, b){ return a.titulos[2].qtd < b.titulos[2].qtd});
 }
 
 //Exercício 2.A
 function somarPorNacionais(arr){
-  for(var i = 0, sum = 0; i < arr.length; i++){
+  var sum = 0;
+  for(var i in arr){
     sum += arr[i].titulos[0].qtd;
   }
   return sum;
@@ -50,16 +25,18 @@ function somarPorNacionais(arr){
 
 //Exercício 2.B
 function somarPorContinentais(arr){
-  for(var i = 0, sum = 0; i < arr.length; i++){
-    sum += arr[i].titulos[0].qtd;
+  var sum = 0;
+  for(var i in arr){
+    sum += arr[i].titulos[1].qtd;
   }
   return sum;
 };
 
 //Exercício 2.C
 function somarPorTodosTitulos(arr){
-  for(var i = 0, sum = 0; i < arr.length; i++){
-    sum += arr[i].titulos[0].qtd + arr[i].titulos[1].qtd + arr[i].titulos[2].qtd;
+  var sum = 0;
+  for(var i in arr){
+    sum += somarTodosTitulosDoClube(arr[i]);
   }
   return sum;
 };
