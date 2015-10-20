e2b6f63e57163a4aaadee1f1d3676af3f3a87715
Renan Vaz <renanp.vaz@outlook.com>
2015-10-20 20:53:35 -0200

    feat(lista-js-OO): modelar classes carrinhoDeCompras e item

---
+++ src/modulo-03-web/dia-03/lista-js-OO.js
@@ -0,0 +1,10 @@
+function carrinhoDeCompras(){
+  this.itens = new Array();
+}
+
+function item(sku, descricao, quantidade e valorUnitario){
+  this.sku = sku;
+  this.descricao = descricao;
+  this.quantidade = quantidade;
+  this.valorUnitario = valorUnitario;
+}
