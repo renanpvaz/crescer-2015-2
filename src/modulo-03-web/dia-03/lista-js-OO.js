25eef8ad348cda3f6391c4da3e7b36cc6f9ae80d
Renan Vaz <renanp.vaz@outlook.com>
2015-10-20 21:19:22 -0200

    feat(lista-js-OO): adicionar funções para adição e remoção de itens

--- src/modulo-03-web/dia-03/lista-js-OO.js
+++ src/modulo-03-web/dia-03/lista-js-OO.js
@@ -2,9 +2,17 @@ function carrinhoDeCompras(){
   this.itens = new Array();
 }
 
-function item(sku, descricao, quantidade e valorUnitario){
+function item(sku, descricao, quantidade, valorUnitario){
   this.sku = sku;
   this.descricao = descricao;
   this.quantidade = quantidade;
   this.valorUnitario = valorUnitario;
+}
+
+carrinhoDeCompras.prototype.adicionarItem = function(item){
+  this.itens.push(item);
+}
+
+carrinhoDeCompras.prototype.removerItem = function(sku){
+  this.itens = this.itens.filter(function(elem, itens) { elem.sku != sku});
 }
