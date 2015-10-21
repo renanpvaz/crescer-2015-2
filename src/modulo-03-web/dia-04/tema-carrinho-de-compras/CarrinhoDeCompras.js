36ecae4d47cf48dc1eb416393ad7d3ac8fb55106
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-10-21 15:21:43 -0200

    feat(forcarCompra, concluirPedido): adicionar método que aumenta preço a cada 5 segundos e outro que conclui o pedido.

--- src/modulo-03-web/dia-04/tema-carrinho-de-compras/CarrinhoDeCompras.js
+++ src/modulo-03-web/dia-04/tema-carrinho-de-compras/CarrinhoDeCompras.js
@@ -44,4 +44,24 @@ CarrinhoDeCompras.prototype.sortearDesconto = function() {
   return sorte;
 };
 
-// }
+CarrinhoDeCompras.prototype.forcarCompra = function() {
+  if (!this.intervalo) {
+    // var self = this;
+    // self.itens
+    // guardamos o id do intervalo em uma propriedade do objeto (em memória)
+    // para poder cancelá-lo posteriormente
+    this.intervalo = setInterval(function() {
+      this.itens.forEach(function(elem) {
+        console.log('antes: ', elem.valorUnitario);
+        elem.valorUnitario += elem.valorUnitario * .1;
+        console.log('depois: ', elem.valorUnitario);
+      });
+    }.bind(this), 5000);  
+  }
+};
+
+CarrinhoDeCompras.prototype.concluirPedido = function() {
+  clearInterval(this.intervalo);
+  // remover propriedade intervalo do objeto
+  delete this.intervalo;
+};
