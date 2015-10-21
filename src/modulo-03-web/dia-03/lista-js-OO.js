6e6e5776077d68707d9549e8899784d1290cec64
Renan Vaz <renanp.vaz@outlook.com>
2015-10-21 14:16:29 -0200

    feat(lista-js-OO): adicionar função forcarCompra

--- src/modulo-03-web/dia-03/lista-js-OO.js
+++ src/modulo-03-web/dia-03/lista-js-OO.js
@@ -1,38 +1,49 @@
-function carrinhoDeCompras(){
+function CarrinhoDeCompras(){
   this.itens = new Array();
 }
 
-function item(sku, descricao, quantidade, valorUnitario){
+function Item(sku, descricao, quantidade, valorUnitario){
   this.sku = sku;
   this.descricao = descricao;
   this.quantidade = quantidade;
   this.valorUnitario = valorUnitario;
 }
 
-carrinhoDeCompras.prototype.adicionarItem = function(item){
+CarrinhoDeCompras.prototype.adicionarItem = function(item){
   this.itens.push(item);
 }
 
-carrinhoDeCompras.prototype.removerItem = function(sku){
-  this.itens = this.itens.filter(function(elem, itens) { elem.sku != sku});
+CarrinhoDeCompras.prototype.removerItem = function(sku){
+  this.itens = this.itens.filter(function(elem, itens) { elem.sku !== sku});
 }
 
-carrinhoDeCompras.prototype.atualizarQuantidade = function(sku, qtd){
+CarrinhoDeCompras.prototype.atualizarQuantidade = function(sku, qtd){
   this.itens.forEach(function (elem){ if(elem.sku === sku){ elem.qtd = qtd;}});
 }
 
-item.prototype.calcularSubTotal = function(){
+Item.prototype.calcularSubTotal = function(){
   return this.quantidade * this.valorUnitario;
 }
 
-carrinhoDeCompras.prototype.calcularTotal = function(){
+CarrinhoDeCompras.prototype.calcularTotal = function(){
   return this.itens
   .reduce(function(sum, elem){ return sum + elem.calcularSubTotal()} ,0);
 }
 
-carrinhoDeCompras.prototype.sortearDesconto = function(){
+CarrinhoDeCompras.prototype.sortearDesconto = function(){
   var sorte = Math.floor(Math.random() * 10) + 1;
   if(sorte <= 4){
-    this.itens.forEach(function(elem){ elem.valorUnitario *= 0.9})
+    this.itens.forEach(function(elem){ elem.valorUnitario *= 0.9; });
   }
+}
+
+CarrinhoDeCompras.prototype.forcarCompra = function(){
+  var carrinho = this;
+  setInterval(
+    function(){
+      carrinho.itens.forEach(function(elem){
+        elem.valorUnitario += elem.valorUnitario * 0.1;
+        console.log(elem.valorUnitario);
+      }
+    )}, 5000);
 }
