7fb4a901e64f5e82de8e0f72cf37e64c6bb39525
Fabrício Rissetto <fabriciorissetto@gmail.com>
2015-10-26 16:24:27 -0200

    Incluindo exemplo de console application

---
+++ src/modulo-04-c-sharp/dia-01/ExemploEmAula/ConsoleApp/Agenda.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    public class Agenda
+    {
+        private List<Contato> contatos = new List<Contato>();        
+
+        public int QuantidadeContatos { get { return contatos.Count; } }
+
+        public void AdicionarContato(Contato contato)
+        {
+            contatos.Add(contato);
+        }
+
+        //public void RemoverContato(string nomeContato)
+        //{
+        //    foreach (var contato in contatos)
+        //    {
+        //        if (contato.Nome == nomeContato)
+        //        {
+        //            contatos.Remove(contato);
+        //            break;
+        //        }
+        //    }
+        //}
+
+        public void RemoverContato(string nomeContato)
+        {
+            var contatosASeremRemovidos = new List<Contato>();
+
+            for (int i = 0; i < contatos.Count; i++)
+            {
+                if (contatos[i].Nome == nomeContato)
+                    contatosASeremRemovidos.Add(contatos[i]);
+            }
+
+            foreach (var contato in contatosASeremRemovidos)
+            {
+                contatos.Remove(contato);
+            }
+        }
+
+        public void ListarContatos()
+        {
+            foreach (var contato in contatos)
+            {
+                Console.WriteLine(contato.Nome + "-" + contato.Numero);
+            }
+        }
+    }
+}
