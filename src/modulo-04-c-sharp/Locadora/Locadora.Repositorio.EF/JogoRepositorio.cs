651c52fafa1dd54a75a5d576c506511c988892df
renanpvaz <renanp.vaz@outlook.com>
2015-11-09 23:25:06 -0200

    feat JogoRepositorio e métodos para buscar por nome ou id

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/JogoRepositorio.cs
@@ -0,0 +1,51 @@
+﻿using Locadora.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Repositorio.EF
+{
+    class JogoRepositorio
+    {
+
+        public int Atualizar(Jogo jogo)
+        {
+            return 1;
+        }
+
+        public Jogo BuscarPorId(int id)
+        {
+            using (var db = new BaseDeDados())
+            {
+                return db.Jogo.Find(id);
+            }
+        }
+
+        public IList<Jogo> BuscarPorNome(string nome)
+        {
+            using (var db = new BaseDeDados())
+            {
+                return db.Jogo.Where(j => j.Nome.Contains(nome)).ToList();
+            }
+        }
+
+        public IList<Jogo> BuscarTodos()
+        {
+            return null;
+        }
+
+        public int Criar(Jogo jogo)
+        {
+            return 1;
+        }
+
+        public int Excluir(int id)
+        {
+            return 1;
+        }
+
+
+    }
+}
