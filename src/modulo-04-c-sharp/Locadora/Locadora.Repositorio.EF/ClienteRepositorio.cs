b38e8842a523e64c58ff517db5213cfd701ec2b2
renanpvaz <renanp.vaz@outlook.com>
2015-11-09 23:56:41 -0200

    feat ClienteRepositorio

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/ClienteRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using Locadora.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Repositorio.EF
+{
+    class ClienteRepositorio
+    {
+
+        public Cliente BuscarPorId(int id)
+        {
+            using (var db = new BaseDeDados())
+            {
+                return db.Cliente.Find(id);
+            }
+        }
+
+        public IList<Cliente> BuscarPorNome(string nome)
+        {
+            using (var db = new BaseDeDados())
+            {
+                return db.Cliente.Where(j => j.Nome.Contains(nome)).ToList();
+            }
+        }
+
+    }
+}
