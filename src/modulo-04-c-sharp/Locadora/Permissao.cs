b3e6e9e419e0ecfea76fc04e0010a5028c8d30e1
Renan Vaz <renanp.vaz@outlook.com>
2015-11-11 13:31:16 -0200

    adicionar permissao e usuario

---
+++ src/modulo-04-c-sharp/Locadora/Permissao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Permissao
+{
+
+    public string Nome { get; set; }
+
+    public ICollection<Usuario> Usuarios { get; set; }
+
+    public Permissao()
+	{
+	}
+}
---
+++ src/modulo-04-c-sharp/Locadora/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Usuario
+{
+    public string Nome { get; set; }
+
+    public string Senha { get; set; }
+
+    public string Email { get; set; }
+
+    public ICollection<Permissao> Permissoes { get; set; }
+
+    public Usuario()
+	{
+	}
+}
