48f8a80dc0053bf874cd6edbdea119a5d784f9b8
renanpvaz <renanp.vaz@outlook.com>
2015-11-12 02:00:01 -0200

    feat vídeo é exibido e fundo escurece ao clicar em botão

--- src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.ADO/SQL/02 - PopularJogos.sql
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.ADO/SQL/02 - PopularJogos.sql
@@ -28,3 +28,17 @@ INSERT INTO dbo.Jogo VALUES ('Mickey to Donald: Magical Adventure 3',32.9,3, nul
 insert into Selo (IDSelo, Nome) values (1, 'Ouro');
 insert into Selo (IDSelo, Nome) values (2, 'Prata');
 insert into Selo (IDSelo, Nome) values (3, 'Bronze');
+
+--update jogo set imagem = '/Content/Images/ken.png' where id = 7;
+
+--update jogo set imagem = '/Content/Images/megaman.png' where id = 3;
+
+--update jogo set imagem = '/Content/Images/metroid.png' where id = 4;
+
+--update jogo set descricao = 'Chrono Trigger traz a jogabilidade de RPGs clássicos com diversas inovações. O jogador controla o protagonista e seus companheiros pelo mundo fictício do jogo, composto por diversas florestas, cidades e cavernas. A navegação ocorre através de um mapa aéreo de visão do mundo.' where id = 1
+
+--update jogo set descricao = 'Apesar da storyline e dos personagens diferentes, o modo de jogo é bem similar à série original de Mega Man, com diversas exceções - mais notavelmente: X pode aderir-se às paredes e, do segundo jogo em diante, Zero é armado com o Z-Saber (uma espécie de Sabre de luz, similar ao da série Star Wars) além do mais que tradicional canhão de plasma.' where id = 3
+
+--update jogo set descricao = 'O jogo é do estilo plataforma, com elementos de ação e aventura. Nele, você controla a heroína Samus Aran, que vai até um planeta recuperar um Metroid roubado pelos Piratas do Espaço. Durante sua aventura, Samus encontrará aperfeiçoamentos para sua armadura ao mesmo tempo em que procura os chefes e seu objetivo final, o Metroid.' where id = 4
+
+--update jogo set descricao = 'Assim como Street Fighter IV, Super Street Fighter IV traz cenários 3D e personagens controláveis em plano 2D. Os "Focus Attacks" e barras diferentes para "Super" e "Ultra" combos retornam. A barra de "Ultra Combo" não sofreu alterações em relação a Street Fighter IV, mas os jogadores podem escolher entre dois Ultra Combos para cada personagem.' where id = 7
