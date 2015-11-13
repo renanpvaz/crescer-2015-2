18a567eb38be3c5b0dde3d4dc49627a985b84638
Renan Vaz <renanp.vaz@outlook.com>
2015-11-13 13:57:47 -0200

    adicionar testes

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Dominio.Test/LocacaoTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Locadora.Dominio.Test
+{
+    [TestClass]
+    public class LocacaoTest
+    {
+        [TestMethod]
+        public void CriadaLocacaoComClienteEJogo()
+        {
+            var cliente = new Cliente(1);
+            var jogo = new Jogo();
+
+            var locacao = new Locacao(jogo, cliente);
+
+            Assert.AreEqual(jogo, locacao.Jogo);
+            Assert.AreEqual(cliente, locacao.Cliente);
+        }
+
+        [TestMethod]
+        public void LocacaoEhFinalizada()
+        {
+            var locacao = new Locacao(1, 1);
+
+            locacao.RealizarEntrega();
+
+            Assert.AreEqual(DateTime.Now, locacao.DataEntrega);
+        }
+    }
+}
---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Dominio.Test/UsuarioTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Locadora.Dominio.Test
+{
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void UsuarioPossuiPermissaoAdicionada()
+        {
+            var permissao = new Permissao();
+            var usuario = new Usuario();
+
+            permissao.Nome = "PERMISSAO_QUE_PODE_TUDO";
+            usuario.Permissoes.Add(permissao);
+
+            Assert.IsTrue(usuario.Permissoes.Contains(permissao));
+        }
+
+        [TestMethod]
+        public void UsuarioPossuiAmbasPermissoesAdicionadas()
+        {
+            var permissao1 = new Permissao();
+            var permissao2 = new Permissao();
+            var usuario = new Usuario();
+
+            permissao1.Nome = "PERMISSAO_QUE_PODE_TUDO";
+            permissao2.Nome = "PERMISSAO_DO_FABRICIO_OPERADOR";
+            usuario.Permissoes.Add(permissao1);
+            usuario.Permissoes.Add(permissao2);
+
+            Assert.AreEqual(2, usuario.Permissoes.Count);
+        }
+    }
+}
