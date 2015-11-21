90f7f9374a203b3fba6fd8d454a7d5f93a02e961
renanpvaz <renanp.vaz@outlook.com>
2015-11-20 22:29:30 -0200

    adicionar projeto original lavanderia

---
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/LavanderiaApplication.java
@@ -0,0 +1,12 @@
+package br.com.cwi.crescer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavanderiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LavanderiaApplication.class, args);
+    }
+}
---
+++ src/modulo-05-java/lavanderia/src/test/java/br/com/cwi/crescer/LavanderiaApplicationTests.java
@@ -0,0 +1,18 @@
+package br.com.cwi.crescer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LavanderiaApplication.class)
+@WebAppConfiguration
+public class LavanderiaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
