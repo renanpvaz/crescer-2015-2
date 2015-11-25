18680e9afb741f77dac3d74e15dc786ab817f033
Renan Vaz <renanp.vaz@outlook.com>
2015-11-25 21:03:35 -0200

    estilizar tela de erro 404

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/LavanderiaApplication.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/LavanderiaApplication.java
@@ -25,9 +25,10 @@ public class LavanderiaApplication {
 
                 ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
                 ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+                ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, "/404.html");
                 ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
 
-                container.addErrorPages(error401Page, error404Page, error500Page);
+                container.addErrorPages(error401Page, error400Page, error404Page, error500Page);
             }
         };
     }
