0e4a17bac981977c1062dde4c3f4d31e5d96282b
Renan Vaz <renanp.vaz@outlook.com>
2015-11-28 14:38:44 -0200

    feat listagem de produtos

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/dto/ProdutoDTO.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/dto/ProdutoDTO.java
@@ -6,6 +6,7 @@ import br.com.cwi.crescer.domain.Produto;
 
 public class ProdutoDTO {
 
+	private Long id;
     private String material;
     private String servico;
     private BigDecimal valor;
@@ -21,11 +22,60 @@ public class ProdutoDTO {
     }
 
     public ProdutoDTO(Produto entity) {
+    	this.id = entity.getIdProduto();
         this.material = entity.getMaterial().getDescricao();
         this.servico = entity.getServico().getDescricao();
         this.valor = entity.getValor();
         this.prazo = entity.getPrazo();
         this.situacao = entity.getSituacao().toString();
     }
+   
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public String getServico() {
+		return servico;
+	}
+
+	public void setServico(String servico) {
+		this.servico = servico;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Long getPrazo() {
+		return prazo;
+	}
+
+	public void setPrazo(Long prazo) {
+		this.prazo = prazo;
+	}
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
 }
+
--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/service/ProdutoService.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/service/ProdutoService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.cwi.crescer.dao.ProdutoDAO;
+import br.com.cwi.crescer.domain.Cliente;
 import br.com.cwi.crescer.domain.Produto;
+import br.com.cwi.crescer.dto.ClienteResumoDTO;
 import br.com.cwi.crescer.dto.ProdutoDTO;
 
 @Service
@@ -23,8 +25,12 @@ public class ProdutoService {
     public Object listarTodos() {
         List<Produto> produtos = produtoDAO.findAll();
         List<ProdutoDTO> produtosDTO = new ArrayList<ProdutoDTO>();
+        
+        for (Produto produto: produtos) {
+        	produtosDTO.add(new ProdutoDTO(produto));
+        }
 
-        return null;
+        return produtosDTO;
     }
 
 }
